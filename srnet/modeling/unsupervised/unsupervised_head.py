d1b92108ce7670c1f6b0aacceb307c02d3cea1b8
sean rice <contact@sean.ee>
2020-08-12 00:37:19 -0400

    add ImageDecoder

--- srnet/modeling/unsupervised/unsupervised_head.py
+++ srnet/modeling/unsupervised/unsupervised_head.py
@@ -2,6 +2,7 @@ from abc import abstractmethod
 import logging
 from typing import Any, Dict, Iterable, Optional
 
+from detectron2.config import CfgNode
 from detectron2.layers import ShapeSpec
 from detectron2.utils.registry import Registry
 import torch
@@ -19,16 +20,32 @@ logger = logging.getLogger(__name__)
 
 
 class UnsupervisedHead(torch.nn.Module):
-    @abstractmethod
-    def into_per_item_iterable(
-        self, network_output: Dict[str, torch.Tensor], **kwargs: Dict[str, Any]
-    ) -> Iterable:
-        pass
+    """
+    An unsupervised head that accepts feature maps from the backbone and
+    carries out some sort of additional objective.
+
+    NOTE: This abstract class is nice to have for reference, but to achieve an
+    adequateamount of flexibility to subclasses, the interface isn't really
+    well-defined enough to typecheck. Concrete implementations of functions
+    will probably have to disable typechecking for their signatures.
+    """
 
     @abstractmethod
     @classmethod
-    def postprocess(cls, **kwargs: Dict[str, Any]) -> Any:
-        pass
+    def from_config(cls, cfg: CfgNode, *args: Any, **kwargs: Any) -> Dict[str, Any]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def into_per_item_iterable(
+        cls, network_output: Dict[str, torch.Tensor]
+    ) -> Iterable:
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def postprocess(cls, *args: Any, **kwargs: Any) -> Any:
+        raise NotImplementedError()
 
 
 def build_unsupervised_head(
