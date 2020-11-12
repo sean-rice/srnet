ab0123b06379148518c61473a7fb5ac7722a395b
sean rice <contact@sean.ee>
2020-11-12 01:12:00 -0500

    add environment file; update dev tooling

--- dev/lint.sh
+++ dev/lint.sh
@@ -1,4 +1,5 @@
 autoflake -i -r --ignore-init-module-imports --remove-all-unused-imports .
-isort -sp ./setup.cfg -rc -y .
+# for isort<5.0.0: isort -sp ./setup.cfg -rc -y .
+isort --sp ./setup.cfg .
 black -l 88 -t py38 .
-mypy --config-file ./setup.cfg .
+mypy --config-file ./setup.cfg . | grep -v 'found module but no type hints or library stubs'
