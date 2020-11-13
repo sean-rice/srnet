fe429eaa97a209098b7cb11c42ad7329f588df75
sean rice <contact@sean.ee>
2020-11-12 20:25:34 -0500

    add isort version check to linting script

--- dev/lint.sh
+++ dev/lint.sh
@@ -1,5 +1,14 @@
 autoflake -i -r --ignore-init-module-imports --remove-all-unused-imports .
-# for isort<5.0.0: isort -sp ./setup.cfg -rc -y .
+
+# isort version check & run
+ISORT_VERSION=$(isort --version-number)
+if [[ "$ISORT_VERSION" != 5* ]]; then
+    echo "error: linter requires isort=5"
+    exit 1
+fi
 isort --sp ./setup.cfg .
+
 black -l 88 -t py38 .
-mypy --config-file ./setup.cfg . | grep -v 'found module but no type hints or library stubs'
+mypy --config-file ./setup.cfg . \
+    | grep -v 'found module but no type hints or library stubs' \
+    | grep -v 'note: See https://mypy.readthedocs.io/en/latest/running_mypy.html#missing-imports'
