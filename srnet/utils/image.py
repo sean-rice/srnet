7eb31173c2a9ccd4f41126953203dc5453f5026c
sean <dev@sean.ee>
2024-03-20 00:22:55 -0700

    fix pillow 10.x deprecations
    closes #2

--- srnet/utils/image.py
+++ srnet/utils/image.py
@@ -9,11 +9,11 @@ def resize_with_pil(
     example: Union[np.ndarray, torch.Tensor],
     h: int,
     w: int,
-    mode: Any = Image.ANTIALIAS,
+    mode: Any = Image.LANCZOS,
 ) -> np.ndarray:
     """
     Resizes an `ndarray` or `Tensor` image (h, w [,c]) with PIL using the
-    specified mode (with the high-quality `Image.ANTIALIAS` default), and
+    specified mode (with the high-quality `Image.LANCZOS` default), and
     returns it as an `ndarray`.
     """
     img: np.ndarray
